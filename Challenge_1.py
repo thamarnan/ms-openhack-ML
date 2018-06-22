0e5debc94ea477fb8419dd8af719d2f619cbd022
Anan Roderick <thamarnan@gmail.com>
2018-06-22 11:17:22 -0700

    Testing python, numpy and json POST request

--- Challenge_1.py
+++ Challenge_1.py
@@ -11,10 +11,10 @@ get_ipython().system(' /anaconda/envs/py35/bin/python -m pip install --yes numpy
 import requests 
 
 headers = dict()
-headers['Prediction-Key'] = '1812e5ddea424989a9ead7a92e83a51e'
+headers['Prediction-Key'] = 'xxx'
 headers["Content-Type"] = 'application/json'
 
-r = requests.post('https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/a1ba6f7d-57b4-40d3-ba55-f013e7e4fac4/url?iterationId=f63dfbcc-466d-4db0-a18c-436037ce4151')
+r = requests.post('https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/xx/url?iterationId=xx')
 
 print(r.status_code)
 
